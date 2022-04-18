e1c743c17054f4817a2b56ad69c9de8694f4b531
efrainmpp1 <efrainmpp@hotmail.com>
2022-04-18 18:02:44 -0300

    Inicializando Model de User

--- api-node/src/database/index.js
+++ api-node/src/database/index.js
@@ -1,10 +1,12 @@
 const { Sequelize } = require("sequelize")
 const db_config = require("../config/config")
+const User = require("../Models/User")
 
 const connection = new Sequelize(db_config)
 
 connection.authenticate()
 .then(() => {
+  User.init(connection)
   console.log("Conexão com DB realizada com sucesso")
 }).catch(() => {
   console.log("A conexão com o DB não pode ser realizada");
