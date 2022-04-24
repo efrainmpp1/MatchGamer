a12ee7e82bd6ba7ad111433d31415ca0211046c3
efrainmpp1 <efrainmpp@hotmail.com>
2022-04-24 17:36:13 -0300

    editando nome de rota de user

--- api-node/src/routes.js
+++ api-node/src/routes.js
@@ -8,7 +8,7 @@ routes.get('/' , (req,res) => {
 })
 
 routes.get('/users' , UserControllers.readAll)
-routes.get('/user/:id' , UserControllers.readOne)
+routes.get('/users/:id' , UserControllers.readOne)
 routes.post('/users' , UserControllers.cadastrar)
 routes.put('/user/:id' , UserControllers.update)
 routes.delete('/user/:id' , UserControllers.delete)
