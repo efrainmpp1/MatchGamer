d847ebd14c8315c2b9b611d6a6356af4f69c5ccc
efrainmpp1 <efrainmpp@hotmail.com>
2022-05-14 13:20:56 -0300

    Começando front em React

---
+++ front-web/src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React  from 'react';
+import {Navbar , Nav , Container} from 'react-bootstrap'
+
+function NavBar() {
+  return(
+    <Navbar bg="dark" variant="dark" >
+      <Container>
+        <Navbar.Brand href="#home">Match Gamer</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
